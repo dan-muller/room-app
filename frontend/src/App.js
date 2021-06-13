4843806f0415cff5e840244de4b89f9286d86d4e
dan-muller <dmuller720@gmail.com>
2021-06-13 14:25:10 -0400

    Adds new behavior which routs based on stage name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [body, setBody] = React.useState(["Hello World"]);
+  const ws = new WebSocket("/ws/");
+  ws.onmessage = (ev) => {
+    setBody([...body, ev]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        {body.map((text) => (
+          <div>{text}</div>
+        ))}
+      </body>
     </div>
   );
 }
