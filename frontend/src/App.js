6714a11769458c826f860bd4a1bf68e74ae292c8
Daniel Muller <dmuller@jooraccess.com>
2021-06-16 11:07:10 -0400

    use JS std WebSocket

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
 import React from "react";
-import { io } from "socket.io-client";
 
 function useBody() {
   const [body, setBody] = React.useState(["Hello World"]);
   console.debug("Body:", body);
-  // const url = window.location.href.replace("https", "wss") + "/ws/";
   const url = `wss://${window.location.host}/ws/`;
   console.debug("WS URL:", url);
-  const ws = io(url);
-  ws.onAny((event) => {
+  const ws = new WebSocket(url);
+  ws.onmessage = (event) => {
     console.debug(event);
     setBody([...body, event]);
-  });
-  ws.send("Hello WS, I have connected.");
+  };
+  ws.onopen = (ws, event) => {
+    console.debug(event);
+    ws.send("Hello WS, I have connected.");
+  };
   return (
     <>
       {body.map((text) => (
