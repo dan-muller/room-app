5fb8ef9c44c1ec28d147bd5e3250c69ffbda64ac
dan-muller <dmuller720@gmail.com>
2021-07-01 21:23:52 -0400

    Get RoomCode from query param, send it to ws

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,23 +4,38 @@ import React from "react";
 const App = () => {
   const [body, setBody] = React.useState([]);
   console.debug("Body:", body);
-  const url = `wss://${window.location.host}/ws/`;
+
+  const params = Object.fromEntries(
+    window.location.search
+      .replace("?", "")
+      .split("&")
+      .map((param) => param.split("="))
+  );
+
+  const { RoomCode } = params;
+
+  const url = `wss://${window.location.host}/ws/?RoomCode=${RoomCode}`;
   console.debug("WS URL:", url);
   const ws = new WebSocket(url);
+
   ws.onmessage = (event) => {
     console.debug("onmessage", event);
     setBody([...body, event]);
   };
+
   ws.onopen = (event) => {
     console.debug("onopen", event);
     ws.send("Hello WS, I have connected.");
   };
+
   ws.onclose = (event) => {
     console.debug("onclose", event);
   };
+
   ws.onerror = (event) => {
     console.debug("onerror", event);
   };
+
   return (
     <div className="App">
       <body>
