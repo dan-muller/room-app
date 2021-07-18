bbf27e7747a9aa228eaf5b693da6fc9094b4df43
dan-muller <dmuller720@gmail.com>
2021-07-18 17:22:14 -0400

    Better FE appearance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,85 @@
 import "./App.css";
 import React from "react";
+import logo from "./logo.svg";
 
-const App = () => {
-  const [body, setBody] = React.useState(["Events Show up here"]);
-  console.debug("Body:", body);
-
-  const params = Object.fromEntries(
+const useParams = () =>
+  Object.fromEntries(
     window.location.search
       .replace("?", "")
       .split("&")
       .map((param) => param.split("="))
   );
 
+const App = () => {
+  const [body, setBody] = React.useState(["Events Show up here"]);
+  const [error, setError] = React.useState();
+  console.debug("Body:", body);
+  const params = useParams();
+  console.debug("Params:", params);
   const { RoomCode, Name } = params;
+  React.useEffect(() => {
+    if (RoomCode && Name) {
+      const url = `wss://${window.location.host}/ws/?RoomCode=${RoomCode}&Name=${Name}`;
+      console.debug("WS URL:", url);
+      try {
+        const ws = new WebSocket(url);
 
-  if (RoomCode && Name) {
-    const url = `wss://${window.location.host}/ws/?RoomCode=${RoomCode}&Name=${Name}`;
-    console.debug("WS URL:", url);
-    try {
-      const ws = new WebSocket(url);
+        ws.onmessage = (event) => {
+          console.debug("onmessage", event);
+          setBody([...body, event]);
+        };
 
-      ws.onmessage = (event) => {
-        console.debug("onmessage", event);
-        setBody([...body, event]);
-      };
+        ws.onopen = (event) => {
+          console.debug("onopen", event);
+          setBody([...body, JSON.stringify({ onopen: event })]);
+          try {
+            const event = JSON.stringify({
+              body: "Hello WS, I have connected.",
+            });
+            console.log("sending", event);
+            ws.send(event);
+          } catch (e) {
+            console.log("error on send");
+            console.error(e);
+          }
+        };
 
-      ws.onopen = (event) => {
-        console.debug("onopen", event);
-        setBody([...body, event]);
-        try {
-          const event = JSON.stringify({body: "Hello WS, I have connected."})
-          console.log("sending", event)
-          ws.send(event);
-        } catch (e) {
-          console.log("error on send")
-          console.error(e)
-        }
-      };
+        ws.onclose = (event) => {
+          console.debug("onclose", event);
+          setBody([...body, JSON.stringify({ onclose: event })]);
+        };
 
-      ws.onclose = (event) => {
-        console.debug("onclose", event);
-        setBody([...body, event]);
-      };
+        ws.onerror = (event) => {
+          console.debug("onerror", event);
+          setBody([...body, JSON.stringify({ onerror: event })]);
+        };
 
-      ws.onerror = (event) => {
-        console.debug("onerror", event);
-        setBody([...body, event]);
-      };
-
-      const closeWs = () => {
-        if (ws.readyState === WebSocket.OPEN) {
-          ws.close();
-        }
-      };
-      window.addEventListener("unload", closeWs);
-      window.onbeforeunload = closeWs;
-    } catch (e) {
-      console.error(e)
+        const closeWs = () => {
+          if (ws.readyState === WebSocket.OPEN) {
+            ws.close();
+          }
+        };
+        window.addEventListener("unload", closeWs);
+        window.onbeforeunload = closeWs;
+      } catch (e) {
+        console.error(e);
+      }
+    } else {
+      setError("Uh oh! You need a Name and a RoomCode!");
     }
-  } else {
-    setBody([...body, "Uh oh! You need a Name and a RoomCode!"]);
-  }
+  }, [RoomCode && Name]);
 
   return (
     <div className="App">
-      <body>
+      <div className="App-header">
+        {error}
+        <img src={logo} className="App-logo" alt="logo" />
         <ul>
           {body.map((text) => (
-              <li>{text}</li>
+            <li>{text}</li>
           ))}
         </ul>
-      </body>
+      </div>
     </div>
   );
 };
