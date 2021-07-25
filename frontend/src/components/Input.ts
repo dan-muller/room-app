fc050d98712e98db85764b159115ebccc3efc08a
dan-muller <dmuller720@gmail.com>
2021-07-25 17:38:54 -0400

    Use typescript

--- frontend/src/components/Input.ts
+++ frontend/src/components/Input.ts
@@ -1,10 +1,10 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const Button = styled.button`
+export const Input = styled.input`
   padding: 10px 20px;
   margin: 5px 0px;
   font-size: large;
   display: block;
   border: 1px solid lightblue;
   border-radius: 50px;
-`;
+`
