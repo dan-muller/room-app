ee214313904ce4127042ef880030665ca53eabf2
dan-muller <dmuller720@gmail.com>
2021-08-15 23:35:56 -0400

    ts -lint ignore

--- packages/frontend/src/components/Context.tsx
+++ packages/frontend/src/components/Context.tsx
@@ -4,10 +4,8 @@ import { v4 as uuid } from 'uuid'
 import useCookie from 'components/hooks/useCookie'
 import useLocalStorage from 'components/hooks/useLocalStorage'
 
-type Context = {
-  playerId: string
-}
-
+type Context = { playerId: string }
+// eslint-disable-next-line @typescript-eslint/no-redeclare -- intentionally naming the variable the same as the type
 const Context = React.createContext<Context>({ playerId: 'xxxxx' })
 
 export const Provider: React.FC = ({ children }) => {
