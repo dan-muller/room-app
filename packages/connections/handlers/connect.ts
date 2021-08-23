304ce5a4913ae99284e911636936727680b45af3
dan-muller <dmuller720@gmail.com>
2021-08-22 23:54:47 -0400

    add callbacks

--- packages/connections/handlers/connect.ts
+++ packages/connections/handlers/connect.ts
@@ -5,7 +5,7 @@ import parseCookie from 'lib/parseCookie'
 import { BadRequest } from 'lib/error'
 import { OKResponse } from 'lib/response'
 
-const connectHandler: Handler<APIGatewayEvent> = async (event, _context, _callback) => {
+const connectHandler: Handler<APIGatewayEvent> = async (event, _context, callback) => {
   const ConnectionId = event.requestContext.connectionId
   const RoomCode = event.queryStringParameters?.RoomCode
   const { UserId } = parseCookie(event.headers?.Cookie ?? '')
@@ -20,7 +20,7 @@ const connectHandler: Handler<APIGatewayEvent> = async (event, _context, _callba
     logger.log(i)
     const response = new OKResponse(JSON.stringify(i))
     logger.log(response)
-    // callback(null, response)
+    callback(null, response)
     return response
   } else {
     const err = new BadRequest( 'Missing request arguments.', [
@@ -29,7 +29,7 @@ const connectHandler: Handler<APIGatewayEvent> = async (event, _context, _callba
       ...(!UserId ? [`Invalid UserId value: ${UserId}`] : []),
       ...(!UserName ? [`Invalid UserName value: ${UserName}`] : []),
     ].join('. '))
-    // callback(err)
+    callback(err)
     return err.response
   }
 }
