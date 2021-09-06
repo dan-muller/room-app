29e277ec50b1ce53e57664ffc4cf34b24e45aa50
dan-muller <dmuller720@gmail.com>
2021-09-06 09:19:18 -0400

    Add more logging to `publishEvent`

--- packages/connections/clients/connections.ts
+++ packages/connections/clients/connections.ts
@@ -1,25 +1,33 @@
 import api from 'lib/apiGatewayManagementApi'
 import env from 'lib/env'
+import logger from '../lib/logger'
 
-namespace eventsClient {
+namespace connections {
   const endpoint: string = env.get('ENDPOINT')
   if (env.get('NODE_ENV') === 'production' && !endpoint) {
     throw new Error('The environment variable ENDPOINT must be set.')
   }
 
   export const publishEvent = async (ConnectionIds: string[], Event: any) => {
-    console.trace('connections.publishEvent', { ConnectionIds, Event })
-    const Publish = await Promise.all(
-      ConnectionIds.map((ConnectionId) => {
-        api.postToConnection(endpoint, {
-          ConnectionId,
-          Data: Buffer.from(JSON.stringify({ Event })),
-        })
+    console.debug('connections.publishEvent', { ConnectionIds, Event })
+    const PublishEvents = await Promise.all(
+      ConnectionIds.map(async (ConnectionId) => {
+        try {
+          const PostEvent = await api.postToConnection(endpoint, {
+            ConnectionId,
+            Data: Buffer.from(JSON.stringify({ Event })),
+          })
+          logger.trace('connections.publishEvent', { ConnectionId, PostEvent })
+          return PostEvent
+        } catch (Error) {
+          logger.trace('connections.publishEvent', { ConnectionId, Error })
+          return { ConnectionId, Error }
+        }
       })
     )
-    console.trace('connections.publishEvent', { Publish })
-    return Publish
+    console.debug('connections.publishEvent', { ConnectionIds, PublishEvents })
+    return PublishEvents
   }
 }
 
-export default eventsClient
+export default connections
